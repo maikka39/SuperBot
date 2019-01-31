f20d80782958f54cf0052f9667bdbc0f7b7e030b
Maik de Kruif <maikka39@gmail.com>
2019-01-31 17:35:54 +0100

    Restructured file

--- commands/lib/steamapi.py
+++ commands/lib/steamapi.py
@@ -17,20 +17,6 @@ class SteamAPI(object):
     def set_api_key(self, api_key):
         self.api_key = api_key
 
-    def get_game_titles(self):
-        try:
-            with urllib.request.urlopen("https://api.steampowered.com/ISteamApps/GetAppList/v2/") as url:
-                self.steam_titles = safe_get_list(safe_get_list(
-                    json.loads(url.read().decode()), "applist"), "apps")
-        except:
-            self.steam_titles = None
-        return self.steam_titles
-
-    def get_game_title(self, appid):
-        for title in self.steam_titles:
-            if str(safe_get_list(title, "appid")) == str(appid):
-                return safe_get_list(title, "name")
-
     def get_steam_id(self, user):
         match = re.match(
             r'((http|https):\/\/steamcommunity.com\/(profiles|id)\/(?P<steamid>\w+))', user)
@@ -50,26 +36,16 @@ class SteamAPI(object):
                 user = None
         return user
 
-    def get_steam_id_from_url(self, url):
-        match = re.match(r'^(?P<clean_url>https?://steamcommunity.com/'
-                         r'(?P<type>profiles|id|gid|groups)/(?P<value>.*?))(?:/(?:.*)?)?$', url)
+        def get_game_titles(self):
+            try:
+                with urllib.request.urlopen("https://api.steampowered.com/ISteamApps/GetAppList/v2/") as url:
+                    self.steam_titles = safe_get_list(safe_get_list(
+                        json.loads(url.read().decode()), "applist"), "apps")
+            except:
+                self.steam_titles = None
+            return self.steam_titles
 
-        if not match:
-            return None
-
-        web = make_requests_session()
-
-        try:
-            # user profiles
-            if match.group('type') in ('id', 'profiles'):
-                text = web.get(match.group('clean_url'),
-                               timeout=http_timeout).text
-                data_match = re.search(
-                    "g_rgProfileData = (?P<json>{.*?});[ \t\r]*\n", text)
-
-                if data_match:
-                    data = json.loads(data_match.group('json'))
-                    return int(data['steamid'])
-
-        except requests.exceptions.RequestException:
-            return None
+        def get_game_title(self, appid):
+            for title in self.steam_titles:
+                if str(safe_get_list(title, "appid")) == str(appid):
+                    return safe_get_list(title, "name")
