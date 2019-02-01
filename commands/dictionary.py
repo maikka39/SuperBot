848b151f3f4131798fc26fc509823e6cae8b320f
Maik de Kruif <maikka39@gmail.com>
2019-02-01 11:40:08 +0100

    Gave up on PyDictionary... Still looking for replacement

--- commands/dictionary.py
+++ commands/dictionary.py
@@ -7,13 +7,16 @@ from PyDictionary import PyDictionary
 
 COLOR = 0xbb33ee
 
+# TODO: Get other dictionary library
+
 
 class Dictionary:
 
     def __init__(self, bot):  # This allows the cog to access the bot, and its functions
         self.bot = bot
+        self.pydictionary = PyDictionary()
 
-    @commands.command(pass_context=True, aliases=["dict", "define", "def", "meaning"])
+    @commands.command(pass_context=True, aliases=["dict", "dic", "define", "def", "meaning"])
     async def dictionary(self, ctx, word):
         await asyncio.sleep(0.05)
         await ctx.message.delete()
@@ -21,7 +24,7 @@ class Dictionary:
         embed = discord.Embed(
             title="Dictionary", description="Here is what I found for `{}`: \n\n___".format(word), color=COLOR)
 
-        meaning = safe_get_list(PyDictionary(word).getMeanings(), word)
+        meaning = self.pydictionary.meaning(word)
 
         if safe_get_list(meaning, "Noun", False):
             embed.add_field(name="Noun", value=str(
@@ -43,20 +46,11 @@ class Dictionary:
                 '`1.` ' + safe_get_list(safe_get_list(meaning, "Adverb"), 0, "").capitalize() +
                 '\n`2.` ' + safe_get_list(safe_get_list(meaning, "Adverb"), 1, "").capitalize()), inline=False)
 
-        # embed.add_field(name="ID", value=user.id, inline=True)
-
         await ctx.send(embed=embed)
 
-    @commands.command(pass_context=True)
-    async def translate(self, ctx, word):
-        await asyncio.sleep(0.05)
-        await ctx.message.delete()
-        embed = discord.Embed(
-            title="Translator", description="Coming Soon! :tools:", color=COLOR)
-        await ctx.send(embed=embed)
-
-    # TODO: Synonyms
-    # TODO: Antonyms
+    # # TODO: Translations
+    # # TODO: Synonyms
+    # # TODO: Antonyms
 
 
 def setup(bot):
