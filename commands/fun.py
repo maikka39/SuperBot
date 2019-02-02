b0877f0890ff55d8812fbdb1ade809f02ed00895
Maik de Kruif <maikka39@gmail.com>
2019-02-03 00:31:24 +0100

    Added the meme command

--- commands/fun.py
+++ commands/fun.py
@@ -3,9 +3,12 @@ import datetime
 import random
 import secrets
 import sys
+from commands.lib.other import safe_get_list
 
 import discord
+import praw
 from discord.ext import commands
+from mainconf import REDDIT_CLIENT_ID, REDDIT_CLIENT_SECRET, REDDIT_USER_AGENT
 from pythonping import ping
 
 COLOR = 0xff6d00
@@ -15,6 +18,8 @@ class Fun:
 
     def __init__(self, bot):  # This allows the cog to access the bot, and its functions
         self.bot = bot
+        self.reddit = praw.Reddit(
+            client_id=REDDIT_CLIENT_ID, client_secret=REDDIT_CLIENT_SECRET, user_agent=REDDIT_USER_AGENT)
 
     @commands.command(pass_context=True)
     async def ping(self, ctx):
@@ -56,19 +61,25 @@ class Fun:
         await asyncio.sleep(0.05)
         await ctx.message.delete()
 
-        # Send a message with a "real" random number
         embed = discord.Embed(title="Tell the Time", description="The current time is " +
                               str(datetime.datetime.utcnow().time())[:-7] + " UTC", color=COLOR)
         embed.set_footer(text="Requested by {}".format(discord.utils.get(
             self.bot.get_all_members(), id=ctx.message.author.id)), icon_url=ctx.message.author.avatar_url)
         await ctx.send(embed=embed)
 
-    @commands.command(pass_context=True)
+    @commands.command(pass_context=True, aliases=["memes"])
     async def meme(self, ctx):
         await asyncio.sleep(0.05)
         await ctx.message.delete()
         embed = discord.Embed(
-            title="Memes", description="Coming Soon! :tools:", color=COLOR)
+            title="Memes", description="Best memes of today:", color=COLOR)
+        await ctx.send(embed=embed)
+
+        subreddit = self.reddit.subreddit("memes")
+        for submission in subreddit.top(time_filter="day", limit=5):
+            await ctx.send(embed=discord.Embed(color=COLOR).set_image(url=submission.url))
+
+        embed = discord.Embed(description="Check back tomorrow!", color=COLOR)
         embed.set_footer(text="Requested by {}".format(discord.utils.get(
             self.bot.get_all_members(), id=ctx.message.author.id)), icon_url=ctx.message.author.avatar_url)
         await ctx.send(embed=embed)
